b4819423da55997b83732273a93e733b44e4a9b5
fivenames <116367808+fivenames@users.noreply.github.com>
2025-03-05 23:58:08 +0800

    Update code to better meet coding standards

--- src/main/java/Nimbus/NimbusException.java
+++ src/main/java/Nimbus/NimbusException.java
@@ -9,36 +9,29 @@ public class NimbusException extends Throwable {
 
     public void handleException() {
         switch (type) {
-        case "empty command": {
+        case "empty command":
             System.out.println("Empty command, enter a command or enter -help for a list of supported commands");
             break;
-        }
-        case "invalid command word": {
+        case "invalid command word":
             System.out.println("Invalid command word, please try again or enter -help for a list of supported commands");
             break;
-        }
-        case "invalid usage": {
+        case "invalid usage":
             System.out.println("Invalid usage of this command, please try again or enter -help to learn more about the usage");
             break;
-        }
-        case "out of range": {
+        case "out of range":
             System.out.println("Index of the Task selected is out of range, please enter a valid index");
             break;
-        }
-        case "invalid index": {
+        case "invalid index":
             System.out.println("Enter a number to specify the task");
             break;
-        }
-        case "no deadline specified": {
+        case "no deadline specified":
             System.out.println("No deadline specified, specify a deadline using '/by'" +
                     "for example: deadline CS2113 homework /by Friday 2PM");
             break;
-        }
-        case "no event specified": {
+        case "no event specified":
             System.out.println("No duration specified, specify the start timing using '/from' and specify the end timing using '/to'" +
                     "for example: event CS2113 exam /from 2PM /to 4PM");
             break;
-        }
         }
     }
 }
